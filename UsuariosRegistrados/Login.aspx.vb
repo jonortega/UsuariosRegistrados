25c0ddac62579eaff70af70a8363ca3fd487400d
paabloomaartinez <pablo121002@gmail.com>
2023-02-24 20:27:05 +0100

    Poniendolo mejor

--- UsuariosRegistrados/Login.aspx.vb
+++ UsuariosRegistrados/Login.aspx.vb
@@ -6,40 +6,28 @@ Public Class WebForm2
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         lblContraError.Text() = ""
         lblUserError.Text() = ""
+        AccesoDatos.AccesoDatos.Konektatu()
     End Sub
 
     Protected Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
         ' HAY QUE COMPROBAR QUE PUEDE AHCER LOGIN O NO
-        Dim correo As String = Convert.ToString(tbCorreo.Text())
-        Dim contra As String = Convert.ToString(tbContrasenia.Text())
+        Dim correo As String = tbCorreo.Text()
+        Dim contra As String = tbContrasenia.Text()
 
-        AccesoDatos.AccesoDatos.Konektatu()
-
-        Dim user As SqlDataReader = Nothing
-        user = AccesoDatos.AccesoDatos.ErabiltzaileakLortu(correo)
-        Console.WriteLine(user.ToString)
-        Dim pasahitza As String = ""
+        Dim user As SqlDataReader = AccesoDatos.AccesoDatos.ErabiltzaileakLortu(correo)
 
         If user.HasRows Then
             While user.Read()
-                Dim email As String = user("email").ToString()
-                pasahitza = user("pasahitza").ToString()
+                Dim pasahitza As String = user("pasahitza").ToString()
+                If contra = pasahitza Then
+                    Response.Redirect("Menu.aspx")
+                Else
+                    lblContraError.Text() = "Contraseña incorrecta."
+                End If
             End While
-
-            If contra = pasahitza Then
-                Response.Redirect("Menu.aspx")
-            Else
-                lblContraError.Text() = "Contraseña incorrecta."
-            End If
         Else
-            lblUserError.Text() = "Usuario incorrecto"
+            lblUserError.Text() = "No existe el correo."
         End If
-
         AccesoDatos.AccesoDatos.ItxiKonexioa()
-    End Sub
-
-    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Response.Redirect("Register.aspx")
-
     End Sub
 End Class
