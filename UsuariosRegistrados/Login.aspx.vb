799abdee59dac1bb3c656ad14df6f9b571310c08
paabloomaartinez <pablo121002@gmail.com>
2023-02-24 22:16:22 +0100

    Recuperar contra y login finished

--- UsuariosRegistrados/Login.aspx.vb
+++ UsuariosRegistrados/Login.aspx.vb
@@ -49,4 +49,8 @@ Public Class WebForm2
         End If
         AccesoDatos.AccesoDatos.ItxiKonexioa()
     End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Response.Redirect("Register.aspx")
+    End Sub
 End Class
